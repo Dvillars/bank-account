c193a05e9d9441c519a351636bb43e16c8a43085
Derek Villars and Matt Caswell <derek@>
2017-01-24 11:38:13 -0800

    add our first spec

---
+++ README.md
@@ -0,0 +1,16 @@
+specs:
+# Behavior: user enters name and it is prepended to the current balance.
+* input: Matt
+* output: Matt's current balance is:
+
+# Behavior: enter init-deposit and returns given amount for current balance.
+* input: 300
+* output: current balance: 300
+
+# Behavior: enter deposit amount and adds to current balance.
+* input: 50
+* output: current balance: 350
+
+# Behavior: enter withdrawl amount and subtracts from current balance.
+* input: 100
+* output: current balance: 250
---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $("form#new-account").submit(function(event) {
+    event.preventDefault();
+    var accountName = $('#name').val();
+    var intialDeposit = $('#init-deposit').val();
+    $('#result').text("");
+    $("h2").prepend(accountName + "'s ")
+
+  });
+
+  $("form#change-account").submit(function(event) {
+    event.preventDefault();
+    var deposit = $("#deposit").val();
+    var withdrawl = $("#withdrawl").val();
+    var balance = $("#result").val();
+    $("#result").text("");
+  });
+});
